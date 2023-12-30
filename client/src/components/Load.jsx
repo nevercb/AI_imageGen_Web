4d3fa94f50f8d106e98e656ff961ed9e14d15a82
nevercb <903689196@qq.com>
2023-12-30 15:56:22 +0800

    Home page初始化

---
+++ client/src/components/Load.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Load = () => {
+  return <div>Load</div>;
+};
+
+export default Load;
---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+import Card from "./Card";
+import FormField from "./FormField";
+import Load from "./Load";
+
+export { Card, FormField, Load };
